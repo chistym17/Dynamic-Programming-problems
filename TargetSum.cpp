52ba6d289d768aed3a77741f9fd9b77fb19b1e3f
chistym17 <mchisty31@gmail.com>
2023-12-23 23:13:33 +0600

    Target sum added

---
+++ TargetSum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int n, int target) {
+        if (n < 0) {
+            return target == 0 ? 1 : 0;
+        }
+
+  
+
+       int ans= solve(nums, n - 1, target - nums[n]) + solve (nums, n - 1, target + nums[n]);
+
+        return ans;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        return solve(nums, n - 1, target );
+    }
+};
